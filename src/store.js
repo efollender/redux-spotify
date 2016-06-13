972127b9500c740b899cf7e3ce52f37aee05c622
Emma Follender <follender@brooklynunited.com>
2016-06-13 15:25:26 -0400

    Updated socket location

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import remoteActionMiddleWare from './remote_action_middleware';
 import {setState} from './action_creators';
 import io from 'socket.io-client';
 
-const socket = io(`${location.protocol}//${location.hostname}:8090`);
+const socket = io(`http://27c4dcba.ngrok.io`);
 
 socket.on('state', state => {
 	store.dispatch(setState(state))
